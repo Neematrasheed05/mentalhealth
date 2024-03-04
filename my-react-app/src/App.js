881cbc533ec2bcffac5db87d34ddd9e4ecc290e4
mwangiowen <owen31588@gmail.com>
2024-03-04 22:35:26 +0300

    Initial commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,20 +1,19 @@
-import {Routes,Router,Route} from 'react-router-dom'
-import Home from './Components/Home'
-import About from './Components/About'
-import Services from './Components/Services'
-import Navbar from './Components/Navbar'
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Routes path="/home" element={<Home/>}/>
-          <Routes path="/about" element={<About/>}/>
-          <Routes path="/services" element={<Services/>}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </Router>
     </div>
--- my-react-app/src/Components/Navbar.js
+++ my-react-app/src/Components/Navbar.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return (
-        <div>
-            <Link to="/home'>Home</Link>
-            <Link to='/about'> About</Link>
-            <Link to='/services'> Services</Link>
-        </div>
-    );
+  return (
+    <div className="bg-yellow-900 text-yellow-200 py-4 w-full sticky top-0 left-0">
+      <div className="container mx-auto flex justify-center items-center">
+        <Link to="/home" className="text-lg font-bold">
+          Home
+        </Link>
+        <Link to="/about" className="text-lg font-bold ml-20">
+          About
+        </Link>
+        <Link to="/services" className="text-lg font-bold ml-20">
+          Services
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
